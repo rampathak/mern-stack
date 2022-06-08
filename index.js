18646869a596263c0784a726d9d705f2f9806a59
rampathak <ram.pathak65@gmail.com>
2022-06-08 17:28:39 +0545

    first

---
+++ index.js
@@ -0,0 +1,18 @@
+
+var myName;  //initialization
+var myName ="Jon Dpe";  // re-initialization
+var myName ="final John"
+
+//let was introduce in ES6 // in 2015
+let myName2=12
+myName2=1212
+myName2 =131313
+
+//let myName2=1313   //variable defiend with let keyword canot be reclared and only be reinitialized
+const myName3=null
+//myName ="change" //error => assignement to constant varialbe. //variable
+//defined with const cannot be re-initialization
+
+console.log(myName);
+console.log(myName2);
+console.log(myName3);
