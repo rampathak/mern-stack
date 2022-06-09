0f5a3514319a9c79fb9fed1f1e5b08f30fb67d65
rampathak <ram.pathak65@gmail.com>
2022-06-09 16:08:28 +0545

    day3

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
-var myName;  //initialization
+var Test = "CivilAviati";
+
 var myName ="Jon Dpe";  // re-initialization
 var myName ="final John"
 
@@ -9,10 +10,38 @@ myName2=1212
 myName2 =131313
 
 //let myName2=1313   //variable defiend with let keyword canot be reclared and only be reinitialized
-const myName3=null
+const myName3=null;
 //myName ="change" //error => assignement to constant varialbe. //variable
 //defined with const cannot be re-initialization
-
+//console.log(caaan);
 console.log(myName);
 console.log(myName2);
 console.log(myName3);
+console.log(Test);
+
+
+// global variable vs block scope variable vs funcional scope variable
+//variable defined with var keyword are global variable
+//variable defined with let keyword can be global, blocked, functional variable
+//NAMING
+var myName="camelcase"
+// agadi sano ra paxadi thulo letter looks  like  camel so known as camel case
+var my_name="snake_case"
+// whole programming bhari nai yeutai type ko naming convetion use garni either camelcase or  snake case
+//DATA TYPE
+
+//number
+//String
+// array
+//boolean (true or false)
+//object  in javascript not date
+//null
+//undefined
+
+//Advance  JS
+// symbol
+// BigInt
+console.log(typeof(myName));
+
+
+
