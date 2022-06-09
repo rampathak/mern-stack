4546e8c827cb3a801ca75a6881fd648043e50c8e
rampathak <ram.pathak65@gmail.com>
2022-06-09 16:13:53 +0545

    sentfromofficepc

---
+++ day3.js
@@ -0,0 +1,34 @@
+
+// Primitive
+//Number
+//String
+//Non pri
+
+// non primitive (special data type)
+//object(collection)
+//javascritpt object
+let arr=[fruit1, fruit2, age]
+console.log({arr});
+console.log({arr[0]});
+
+    )
+
+
+
+let obj={
+    value: "key"
+}
+
+let value;
+let value2= null;
+let obj2 = {
+    value,
+    value2,
+    arr,
+}
+
+console.log(obj);
+console.log(obj2);
+console.log(obj2.key2);
+
+
---
+++ operator.js
@@ -0,0 +1,53 @@
+//OPERATORS
+//
+//Airthematic   
+
+//Operator	Description 
+// +	Addition
+// -	Subtraction
+// *	Multiplication
+// **	Exponentiation (ES2016)
+// /	Division
+// %	Modulus (Division Remainder)  10%3 =1
+// ++	Increment
+// --	Decrement  */
+//Logical
+//Assignement
+
+//bitwiase
+// comparasion
+
+//post -increment number ++ // returns value before it was incremented
+//per increment number // return value after incormenting
+let num1=100;
+
+console.log(num1++, num1);
+console.log(++num1, num1);
+
+// == decrement
+//logical  // returns only one value i.e boolean //true or false
+//AND  // return true if both conditios is true
+//OR   // return true if any of the condition is ture
+//NOT  // reverse the boolean
+
+let status1 =true
+let status2=true
+let status3=false
+console.log((status1 && status2) && status3);
+console.log("or", status3 || (status1 && status2));
+console.log("not", !status3);
+
+// comparison
+// to check if variable contain the same data
+// == // equals to
+// = // assignement
+//==
+//!=  not equales to  to check if variable s contain the differnt data
+// strictly checks the data -type 
+//===  try garni
+
+
+
+
+
+
