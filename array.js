389dd27d9abbd4cf5d71e63c5231e0abc3fe4eb2
rampathak <ram.pathak65@gmail.com>
2022-06-10 15:09:50 +0545

    day4

---
+++ array.js
@@ -0,0 +1,21 @@
+
+// Non primitive data type
+
+
+let fruit1= "apple";
+let fruit2= "banana";
+let age= 25;
+
+let arr =[fruit1, fruit2];
+console.log({arr});
+// index in array always starts with 0
+console.log(arr[0]);
+
+let address = {
+    'building no': 3960,
+    street: 'North 1st street',
+    state: 'CA',
+    country: 'USA'
+};
+
+console.log(address['building no']);
